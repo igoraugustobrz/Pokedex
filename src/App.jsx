f918cfe6685e31d491cfa9da2b3f51eef80409fe
Igor Augusto <igoraugustobz@gmail.com>
2023-10-14 08:22:42 -0300

    feat: adição do componente Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Banner from "./components/Banner/Banner"
+
 function App() {
 
   return (
     <>
+      <Banner />
     </>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+    return (
+        <>
+            <header className="banner">
+                <img
+                    className="max-w-full"
+                    src="../../../public/img/banner.png"
+                    alt="Banner principal"
+                />
+            </header>
+        </>
+    )
+}
+
+export default Banner;
